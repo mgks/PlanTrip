45c032048fe9a8dd7d0afc78f56bc6dcd00c915a
mgks <hello@mgks.dev>
2025-12-10 02:42:54 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // IMPORTANT: List all files that contain Tailwind classes here
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
